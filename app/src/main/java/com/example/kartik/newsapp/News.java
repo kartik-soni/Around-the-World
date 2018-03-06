176fb3ab5bf42a698f5bb1f940803ad14933d90e
Kartik Soni <sonikartik98@outlook.com>
2018-03-06 22:39:41 +0530

    Array list Adapter created,Array list population function created

---
+++ app/src/main/java/com/example/kartik/newsapp/News.java
@@ -0,0 +1,31 @@
+package com.example.kartik.newsapp;
+
+/**
+ * Created by KARTIK on 3/6/2018.
+ */
+
+public class News {
+    String mTitle;
+    String mAuthor;
+    String mDate;
+    String mSection;
+    String mURL;
+
+    public News(String title,String author,String date,String section,String URL){
+            mTitle=title;
+            mAuthor=author;
+            mDate=date;
+            mSection=section;
+            mURL=URL;
+    }
+
+    public String getTitle(){return mTitle;}
+
+    public String getAuthor(){return mAuthor;}
+
+    public String getDate(){return mDate;}
+
+    public String getSection(){return mSection;}
+
+    public String getURL(){return mURL;}
+}
---
+++ app/src/main/java/com/example/kartik/newsapp/NewsAdapter.java
@@ -0,0 +1,41 @@
+package com.example.kartik.newsapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by KARTIK on 3/6/2018.
+ */
+
+public class NewsAdapter extends ArrayAdapter<News> {
+
+    public NewsAdapter(Context context) {
+        super(context, -1, new ArrayList<News>());
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView
+                    = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent, false);
+        }
+        TextView title = convertView.findViewById(R.id.title);
+        TextView author = convertView.findViewById(R.id.author);
+        TextView date = convertView.findViewById(R.id.date);
+        TextView section = convertView.findViewById(R.id.section);
+
+        News currentNews = getItem(position);
+        title.setText(currentNews.getTitle());
+        author.setText(currentNews.getAuthor());
+        date.setText(currentNews.getDate());
+        section.setText(currentNews.getSection());
+
+        return convertView;
+    }
+}
